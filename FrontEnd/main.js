36ba6a0db16acb9a3b9e81d7b6d755d17ed1c527
Simon Leruth <simonleruth28@gmail.com>
2025-04-10 04:26:36 -0400

    Ajout des logos de suppression des projets ainsi que leur mise en fonction

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -1,15 +1,25 @@
-import {genererProjets, genererBoutonFiltres, activerFiltres, verifierConnexion, afficherModeEdition, afficherBoutonModifier, appelModale,genererProjetModale} from "./works.js";
+import {
+    genererProjets, genererBoutonFiltres, activerFiltres, connexion, 
+    afficherModeEdition, afficherBoutonModifier, appelModale,genererProjetModale,
+    supprimerProjetModale
+} from "./works.js";
 
 const reponseWorks = await fetch("http://localhost:5678/api/works");
 const works = await reponseWorks.json();
 const reponseCategories = await fetch("http://localhost:5678/api/categories");
 const categories = await reponseCategories.json();
+const token = localStorage.getItem("token");
 
 genererProjets(works);
-genererBoutonFiltres(categories);
-activerFiltres(works);
-verifierConnexion();
-afficherModeEdition();
-afficherBoutonModifier();
-appelModale();
-genererProjetModale(works);
+
+if (token) {
+    connexion();
+    afficherModeEdition();
+    afficherBoutonModifier();
+    appelModale();
+    genererProjetModale(works);
+    supprimerProjetModale();
+} else {
+    genererBoutonFiltres(categories);
+    activerFiltres(works);
+}
--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,5 +1,3 @@
-const token = localStorage.getItem("token");
-
 export async function genererProjets (projets) {
     
     // Récupération de l'élément du DOM qui accueillera les projets
@@ -32,125 +30,113 @@ export async function genererProjets (projets) {
 
 export async function genererBoutonFiltres (categoriesFiltres) {
 
-    if (!token) {
-        // Récupération des deux elements du DOM pour pouvoir positionner des filtres a la bonne place sur la page.
-        const sectionPortfolio = document.querySelector("#portfolio");
-        const divGallery = document.querySelector(".gallery");
+    // Récupération des deux elements du DOM pour pouvoir positionner des filtres a la bonne place sur la page.
+    const sectionPortfolio = document.querySelector("#portfolio");
+    const divGallery = document.querySelector(".gallery");
 
-        // Création d'un 'nav' ainsi que le 'ul' pour y mettre des 'li'.
-        const navFiltres = document.createElement("nav");
-        navFiltres.classList.add("filtres");
-        const ulFiltres = document.createElement("ul");
-        navFiltres.appendChild(ulFiltres);
+    // Création d'un 'nav' ainsi que le 'ul' pour y mettre des 'li'.
+    const navFiltres = document.createElement("nav");
+    navFiltres.classList.add("filtres");
+    const ulFiltres = document.createElement("ul");
+    navFiltres.appendChild(ulFiltres);
 
-        // Création du bouton 'Tous' et ajout de la classe active
-        const filtreTous = document.createElement('li');
-        filtreTous.innerText = "Tous";
-        filtreTous.classList.add("active");
-        filtreTous.dataset.id = "0";
-        ulFiltres.appendChild(filtreTous);
+    // Création du bouton 'Tous' et ajout de la classe active
+    const filtreTous = document.createElement('li');
+    filtreTous.innerText = "Tous";
+    filtreTous.classList.add("active");
+    filtreTous.dataset.id = "0";
+    ulFiltres.appendChild(filtreTous);
 
-        // Boucle afin de generer les boutons a partir de l'API
-        for (let i = 0; i < categoriesFiltres.length; i++) {
-            const categorieCourante = categoriesFiltres[i];
+    // Boucle afin de generer les boutons a partir de l'API
+    for (let i = 0; i < categoriesFiltres.length; i++) {
+        const categorieCourante = categoriesFiltres[i];
             
             
-            const filtre = document.createElement('li');
-            filtre.dataset.id = categorieCourante.id;
-            filtre.innerText = `${categorieCourante.name}`;
+        const filtre = document.createElement('li');
+        filtre.dataset.id = categorieCourante.id;
+        filtre.innerText = `${categorieCourante.name}`;
 
-            ulFiltres.appendChild(filtre);
-        }
-        // on insere a un endroit precis les boutons dans le HTML
-        sectionPortfolio.insertBefore(navFiltres, divGallery);
+        ulFiltres.appendChild(filtre);
     }
+    // on insere a un endroit precis les boutons dans le HTML
+    sectionPortfolio.insertBefore(navFiltres, divGallery);
 }
 
 export async function activerFiltres (works) {
 
-    if (!token) {
-        // On recupere tous les boutons dans une seule variable
-        const boutonsFiltres = document.querySelectorAll(".filtres li");
+    // On recupere tous les boutons dans une seule variable
+    const boutonsFiltres = document.querySelectorAll(".filtres li");
 
-        // On boucle sur les boutons afin d'y ajouter un evenement de type click
-        boutonsFiltres.forEach(bouton => {
-            bouton.addEventListener('click', () => {
+    // On boucle sur les boutons afin d'y ajouter un evenement de type click
+    boutonsFiltres.forEach(bouton => {
+        bouton.addEventListener('click', () => {
 
-                // On retire la classe active de tous les boutons afin de mettre la classe sur l'evenement en question
-                boutonsFiltres.forEach(b => b.classList.remove("active"));
-                bouton.classList.add("active");
+            // On retire la classe active de tous les boutons afin de mettre la classe sur l'evenement en question
+            boutonsFiltres.forEach(b => b.classList.remove("active"));
+            bouton.classList.add("active");
 
-                // on recupere l'id du bouton en question
-                const idCategory = parseInt(bouton.dataset.id);
-                let projetsFiltres;
+            // on recupere l'id du bouton en question
+            const idCategory = parseInt(bouton.dataset.id);
+            let projetsFiltres;
 
-                // condition dans laquel si le bouton est le bouton "tous", on laisse tous les works au sinon on filtre 
-                // les works en fonction de l'id de la categorie du work
-                if (idCategory === 0) {
-                    projetsFiltres = works
-                } else {
-                    projetsFiltres = works.filter(work => work.category.id === idCategory);
-                }
+            // condition dans laquel si le bouton est le bouton "tous", on laisse tous les works au sinon on filtre 
+            // les works en fonction de l'id de la categorie du work
+            if (idCategory === 0) {
+                projetsFiltres = works
+            } else {
+                projetsFiltres = works.filter(work => work.category.id === idCategory);
+            }
 
-                // On vide la partie HTML et on refresh avec les works demandes
-                document.querySelector(".gallery").innerText = "";
-                genererProjets(projetsFiltres);
-            })
-        });
-    }
+            // On vide la partie HTML et on refresh avec les works demandes
+            document.querySelector(".gallery").innerText = "";
+            genererProjets(projetsFiltres);
+        })
+    });
 }
 
-
-export function verifierConnexion() {
+export function connexion() {
 
     // On recupere l'element ou doit changer le login/logout
     const loginLink = document.getElementById("loginLink");
 
-    // si il y a token dans le localStorage alors on change le login -> logout et au moment du click sur ce logout on supprime 
+    // On change le login -> logout et au moment du click sur ce logout on supprime 
     // le token du localStorage
-    if (token) {
-        loginLink.textContent = "logout";
+    loginLink.textContent = "logout";
 
-        loginLink.addEventListener('click', () => {
-            localStorage.removeItem("token");
-            window.location.reload();
-        })
-    }
+    loginLink.addEventListener('click', () => {
+        localStorage.removeItem("token");
+        window.location.reload();
+    })
 }
+
 
 export function afficherModeEdition () {
 
-    if (token) {
-        const body = document.querySelector("body");
-        const html = document.querySelector("html");
+    const body = document.querySelector("body");
+    const html = document.querySelector("html");
 
-        const topBar = document.createElement("div");
-        topBar.classList.add("topBar")
-        topBar.innerHTML += '<i class="fa-regular fa-pen-to-square"></i><p>Mode édition</p>';
+    const topBar = document.createElement("div");
+    topBar.classList.add("topBar")
+    topBar.innerHTML += '<i class="fa-regular fa-pen-to-square"></i><p>Mode édition</p>';
 
-        html.insertBefore(topBar, body);
-    }
-
+    html.insertBefore(topBar, body);
 }
 
 export function afficherBoutonModifier () {
 
-    if (token) {
-        const h2 = document.querySelector("#portfolio h2");
-        const div = document.createElement("div");
-        div.classList.add("edition");
-        h2.replaceWith(div);
-        div.appendChild(h2);
+    const h2 = document.querySelector("#portfolio h2");
+    const div = document.createElement("div");
+    div.classList.add("edition");
+    h2.replaceWith(div);
+    div.appendChild(h2);
 
 
-        const editionBouton = document.createElement("a");
-        editionBouton.classList.add("js-modale");
-        editionBouton.setAttribute('href', "#modale1");
-        editionBouton.innerHTML += '<i class="fa-regular fa-pen-to-square"></i><p>Modifier</p>';
+    const editionBouton = document.createElement("a");
+    editionBouton.classList.add("js-modale");
+    editionBouton.setAttribute('href', "#modale1");
+    editionBouton.innerHTML += '<i class="fa-regular fa-pen-to-square"></i><p>Modifier</p>';
 
-        div.appendChild(editionBouton);
-    }
-
+    div.appendChild(editionBouton);
 }
 
 export function appelModale() {
@@ -176,7 +162,7 @@ export function appelModale() {
 
     const closeModale = function (event) {
         if (modale === null) return;
-        
+            
         event.preventDefault();
 
         modale.style.display = "none";
@@ -204,7 +190,6 @@ export function appelModale() {
             closeModale(event);
         }
     })
-       
 }
 
 export function genererProjetModale(works) {
@@ -220,12 +205,41 @@ export function genererProjetModale(works) {
         // Création des balises
         const imageElementModale = document.createElement("img");
         imageElementModale.src = projetElementModale.imageUrl;
-        const titreElementModale = document.createElement("p");
-        titreElementModale.innerText = projetElementModale.title;
+        const lienCorbeille = document.createElement("a");
+        lienCorbeille.classList.add("trash")
+        lienCorbeille.innerHTML = '<i class="fa-solid fa-trash-can"></i>'
 
-        // On rattache la balise article a la section photosGalerie
+        // On rattache les balises a la section photosGalerie
         projetsModale.appendChild(projetModale);
         projetModale.appendChild(imageElementModale);
-        projetModale.appendChild(titreElementModale);
+        projetModale.insertBefore(lienCorbeille, imageElementModale);
+
     }
+}
+
+export function supprimerProjetModale() {
+    const boutonsSuppProjet = document.querySelectorAll(".trash");
+
+    boutonsSuppProjet.forEach(bouton => bouton.addEventListener('click', async () => {
+        const projet = bouton.closest("article");
+        const projetId = projet.dataset.id;
+
+        try {
+            const reponse = await fetch(`http://localhost:5678/api/works/${projetId}`, {
+                method: 'DELETE',
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                },
+            });
+
+            if (reponse.ok) {
+                projet.remove();
+            } else {
+                console.error("Erreur lors de la suppresion de l'element : ", reponse.status);
+            }
+
+        } catch (err) {
+            console.error("Erreur reseau : ", err);
+        }
+    }));
 }
